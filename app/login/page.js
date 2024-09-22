1d64dcb6b92976d8bb86f9897a1cddb55db2493a
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-22 13:40:06 +0300

    Auth_components

---
+++ app/login/page.js
@@ -0,0 +1,24 @@
+import LoginForm from "@/components/auth/loginForm";
+import Link from "next/link";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <section className="h-screen grid place-items-center">
+      <div className="max-w-[450px] w-full mx-auto p-6 border border-gray-700/20 rounded-md">
+        <h4 className="font-bold text-2xl">Sign in</h4>
+
+        <LoginForm />
+
+        <span className="text-center text-xs text-gray-500">
+          Don&apos;t have an account?{" "}
+          <Link href="/register" className="underline hover:text-indigo-600">
+            Register
+          </Link>
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default LoginPage;
