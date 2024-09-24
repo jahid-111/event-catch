7fc211e05997e47b17a36234158ccf1371a0a2f4
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-09-24 17:28:05 +0300

    Resend-user_Enroll_mail

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,13 +1,16 @@
 "use server";
 
+import EmailTemplate from "@/components/payment/EmailTemplate";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { Resend } from "resend";
 
 const {
   createUser,
   findUserByCredential,
   updateInterest,
   updateGoing,
+  getEventById,
 } = require("@/db/queries");
 
 async function registerUser(formData) {
@@ -45,12 +48,40 @@ async function addInterestEvent(eventId, authId) {
 async function addGoingEvent(eventId, user) {
   try {
     await updateGoing(eventId, user?.id);
-
-    revalidatePath("/");
-    redirect("/");
+    await sendEmail(eventId, user);
   } catch (error) {
+    console.log("ERROR", error);
+    throw error;
+  }
+  revalidatePath("/");
+  redirect("/");
+}
+
+async function sendEmail(eventId, user) {
+  try {
+    const event = await getEventById(eventId);
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    const message = `Dear ${user?.name}, you have been successfully registered for the event, ${event?.name}. Please carry this email and your official id to the venue. We are excited to have you here.`;
+
+    const sent = await resend.emails.send({
+      from: "noreply@dev.jahid.io",
+      to: user?.email,
+      subject: "Successfully Registered EventCatch",
+      react: EmailTemplate({ message }),
+    });
+    return sent;
+  } catch (error) {
+    console.error("Error sending email:", error);
     throw error;
   }
 }
 
-export { registerUser, performLogin, addInterestEvent, addGoingEvent };
+export {
+  registerUser,
+  performLogin,
+  addInterestEvent,
+  addGoingEvent,
+  sendEmail,
+};
---
+++ components/payment/EmailTemplate.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmailTemplate = ({ message }) => {
+  return (
+    <div
+      className="text-xl bg-teal-700
+  "
+    >
+      {message}
+    </div>
+  );
+};
+
+export default EmailTemplate;
